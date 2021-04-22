d68eb3c77ffea3b4096e96d89b1d3b81644c80e0
Kelvin Ma <kelvin@kelvin.ma>
2021-04-21 21:57:01 -0700

    setup the view fields

--- app/src/main/java/com/kelvinhanma/videomerger/RecyclerAdapter.kt
+++ app/src/main/java/com/kelvinhanma/videomerger/RecyclerAdapter.kt
@@ -12,6 +12,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.kelvinhanma.videomerger.model.Video
 import com.kelvinhanma.videomerger.util.inflate
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class RecyclerAdapter(private val context: Context, private val videos: List<Video>) :
     RecyclerView.Adapter<RecyclerAdapter.VideoHolder>() {
@@ -27,6 +30,19 @@ class RecyclerAdapter(private val context: Context, private val videos: List<Vid
         val video = videos[position]
         holder.name.text = video.name
 
+        if (video.dateTaken > 0) {
+            val sdf = SimpleDateFormat("dd/MM/yy hh:mm a")
+            val netDate = Date(video.dateTaken)
+            holder.time.text = sdf.format(netDate)
+        } else {
+            holder.time.visibility = View.INVISIBLE
+        }
+
+        if (video.duration > 0) {
+            holder.duration.text = TimeUnit.MICROSECONDS.toSeconds(video.duration.toLong()).toString()
+        } else {
+            holder.duration.visibility = View.INVISIBLE
+        }
         val videoUri = ContentUris.withAppendedId(
             MediaStore.Video.Media.EXTERNAL_CONTENT_URI, video.id
         )
@@ -39,14 +55,18 @@ class RecyclerAdapter(private val context: Context, private val videos: List<Vid
         private var video: Video? = null
         val name: TextView
         val previewImage: ImageView
-
+        val time: TextView
+        val duration: TextView
         init {
             v.setOnClickListener(this)
             name = v.findViewById(R.id.itemName)
             previewImage = v.findViewById(R.id.itemImage)
+            time = v.findViewById(R.id.itemDate)
+            duration = v.findViewById(R.id.itemDuration)
         }
 
         override fun onClick(v: View?) {
+            // TODO play video? or just select it
             Log.d("RecyclerAdapter", "clicked")
         }
     }
