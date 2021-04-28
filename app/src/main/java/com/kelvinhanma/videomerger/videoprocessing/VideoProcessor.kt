5b89baa1b883aeff7efbd9f7be149daa72d41612
Kelvin Ma <kelvin@kelvin.ma>
2021-04-27 20:58:44 -0700

    add ffmpeg and output merged videos

--- app/src/main/java/com/kelvinhanma/videomerger/videoprocessing/VideoProcessor.kt
+++ app/src/main/java/com/kelvinhanma/videomerger/videoprocessing/VideoProcessor.kt
@@ -1,11 +1,21 @@
 package com.kelvinhanma.videomerger.videoprocessing
 
+import android.content.ContentUris
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.net.Uri
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
+import com.arthenica.mobileffmpeg.Config
+import com.arthenica.mobileffmpeg.Config.RETURN_CODE_CANCEL
+import com.arthenica.mobileffmpeg.Config.RETURN_CODE_SUCCESS
+import com.arthenica.mobileffmpeg.FFmpeg
 import com.kelvinhanma.videomerger.model.Video
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
 import java.util.logging.Logger
+
 
 /**
  * Holds code to handle videos
@@ -23,7 +33,8 @@ class VideoProcessor {
                 MediaStore.Video.VideoColumns._ID,
                 MediaStore.Video.VideoColumns.DISPLAY_NAME,
                 MediaStore.Video.VideoColumns.DATE_TAKEN,
-                MediaStore.Video.VideoColumns.DURATION
+                MediaStore.Video.VideoColumns.DURATION,
+                MediaStore.Video.VideoColumns.DATA
             )
 
         val videos: MutableList<Video> = ArrayList()
@@ -47,7 +58,10 @@ class VideoProcessor {
                     cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.DATE_TAKEN))
                 val duration =
                     cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.DURATION))
-                val video = Video(id, name, timestamp, duration)
+                // TODO use contentprovider and fd instead?
+                val filePath =
+                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.DATA))
+                val video = Video(id, name, timestamp, duration, filePath)
                 LOGGER.info(video.toString())
                 videos.add(video)
             }
@@ -55,9 +69,39 @@ class VideoProcessor {
         return videos
     }
 
-    fun mergeSelectedVideos(context: Context, videos: List<Video>) : Video {
+    fun mergeSelectedVideos(context: Context, videos: List<Video>): Video {
         LOGGER.info("Merging Videos: $videos")
-        return Video(1, "Test", 2, 3)
+        val packageDir = context.applicationInfo.dataDir
+        val inputFile = "input_list.txt"
+        val outputVideo = "$packageDir/out.mp4"
+
+        // generate input list first
+        val fileout: FileOutputStream = context.openFileOutput(inputFile, MODE_PRIVATE)
+        val outputWriter = OutputStreamWriter(fileout)
+        val sb = StringBuilder()
+        videos.forEach {
+            sb.appendln(
+                "file \'${it.filePath}\'"
+            )
+        }
+        outputWriter.write(sb.toString())
+        outputWriter.close()
+
+        // ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.wav
+        when (val rc = FFmpeg.execute("-y -f concat -safe -0 -i \"$packageDir/files/$inputFile\" -c copy $outputVideo")) {
+            RETURN_CODE_SUCCESS -> {
+                LOGGER.info("Command execution completed successfully.")
+            }
+            RETURN_CODE_CANCEL -> {
+                LOGGER.info("Command execution cancelled by user.")
+            }
+            else -> {
+                LOGGER.info("Command execution failed with rc=%d and the output below. $rc")
+                Config.printLastCommandOutput(Log.INFO)
+            }
+        }
+
+        return Video(1, "Test", 2, 3, "test")
     }
 
     // TODO write tests
